836ac1df8519100f96bd3846729dd76015b150ac
Ram Limbu <ramb.limbu531@gmail.com>
2016-04-16 22:36:33 +0545

    new changes

--- 09-data-products/Project-assignment/server.R
+++ 09-data-products/Project-assignment/server.R
@@ -1,39 +1,42 @@
-#install.packages("shiny")
 library(shiny)
+library(datasets)
 
-#the computer guesses an integer between
-number <- floor(runif(1,1,101))
-#print(number)
-numberGuessed <- function(guess, number) {
-    returnValue <- "Nothing entered yet."
-    if (guess > 100) {
-        #print(guess)
-        #print(class(guess))
-        #print(class(100))
-        returnValue <- 'Above 100.\nPlease make a selection between 1 and 100.'
-    }
-    else if (guess < 1) {
-        #print(guess)
-        returnValue <- 'Below 1.\nPlease make a selection between 1 and 100.'
-    }
-    else if (guess > number) {
-        returnValue <- 'Higher than the number.'
-    }
-    else if (guess < number) {
-        returnValue <- 'Lower than the number.'
-    }
-    else if (guess == number) {
-        returnValue <- 'Correct!'
-    }
-    returnValue
-}
-shinyServer( 
-    function(input, output) {
-        output$inputValue <- renderPrint({as.numeric(input$guess)})
-        #output$outputValue <- renderText({numberGuessed(input$guess, number)})
-        output$outputValue <- renderText({
-            if (input$goButton == 0) "You have not guessed a number yet"
-            else if (input$goButton >= 1) numberGuessed(as.numeric(input$guess), number)
+mpgData <- mtcars
+mpgData$am <- factor(mpgData$am, labels = c("Automatic", "Manual"))
+
+shinyServer(function(input, output) {
+    
+    formulaText <- reactive({
+        paste("mpg ~", input$variable)
+    })
+    
+    formulaTextPoint <- reactive({
+        paste("mpg ~", "as.integer(", input$variable, ")")
+    })
+    
+    fit <- reactive({
+        lm(as.formula(formulaTextPoint()), data=mpgData)
+    })
+    
+    output$caption <- renderText({
+        formulaText()
+    })
+    
+    output$mpgBoxPlot <- renderPlot({
+        boxplot(as.formula(formulaText()), 
+                data = mpgData,
+                outline = input$outliers)
+    })
+    
+    output$fit <- renderPrint({
+        summary(fit())
+    })
+    
+    output$mpgPlot <- renderPlot({
+        with(mpgData, {
+            plot(as.formula(formulaTextPoint()))
+            abline(fit(), col=2)
         })
-    }
-)
+    })
+    
+})
